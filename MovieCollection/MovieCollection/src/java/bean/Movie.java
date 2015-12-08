4631012a9f23caf71ae9ca2f0307cf17a40206dd
arbogajk <jkaarbogast@gmail.com>
2015-12-08 17:21:17 -0500

    Created this to store title and field of the movie details button clicked

---
+++ MovieCollection/MovieCollection/src/java/bean/Movie.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author joey
+ */
+public class Movie {
+    private static String title;
+    private static String year;
+    
+    public Movie(String movieTitle, String movieYear){
+        title = movieTitle;
+        year = movieYear;
+    }
+    public static String getTitle(){
+        return title;
+    }
+    public static String getYear(){
+        return year;
+    }      
+}
