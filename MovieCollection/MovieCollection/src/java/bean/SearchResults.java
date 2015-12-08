ad6b812181a7c3fc237d44fda93b8939319234af
arbogajk <jkaarbogast@gmail.com>
2015-12-07 21:01:05 -0500

    I created this bean thinking it will be needed to post the results to the search.jsp page.

---
+++ MovieCollection/MovieCollection/src/java/bean/SearchResults.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ * This bean stores the 2 dimensional array of the search results from the search controller
+ * so it can be access by search.jsp
+ * @author joey
+ */
+public class SearchResults {
+    private static String[][] movieResults;    //Two dimensional array to hold the results of the query
+    
+   
+    public SearchResults(String[][] results){
+        movieResults = results;        
+    }
+    public static String[][] getSearchResults(){
+        return movieResults;
+    }
+}
