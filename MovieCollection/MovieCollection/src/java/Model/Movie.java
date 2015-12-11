8328ec8ef0200b20efe6f45bed86098c5652da78
toddag <toddag@localhost>
2015-12-11 11:23:18 -0500

    Documented Movie.java and general.css has updates

--- MovieCollection/MovieCollection/src/java/Model/Movie.java
+++ MovieCollection/MovieCollection/src/java/Model/Movie.java
@@ -14,17 +14,38 @@ public class Movie {
     private static String year;
     private static String[][] reviews;
     
+    /**
+     * Makes a movie object
+     * @param movieTitle title of movie object
+     * @param movieYear year of movie object
+     * @param movieReviews reviews of movie object
+     */
     public Movie(String movieTitle, String movieYear,String[][] movieReviews){
         title = movieTitle;
         year = movieYear;
         reviews = movieReviews;
     }
+    
+    /**
+     * gets title of movie object
+     * @return title of movie object or null
+     */
     public static String getTitle(){
         return title;
     }
+    
+    /**
+     * gets year of movie object
+     * @return year of the movie object or null
+     */
     public static String getYear(){
         return year;
     } 
+    
+    /**
+     * gets movie reviews of movie object
+     * @return the movie reviews of movie object
+     */
     public static String[][] getMovieReviews(){
         return reviews;
     }
